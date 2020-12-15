76295b908ccf38dc812cfb37ca31c7616af32077
Administrator <1246771571@qq.com>
2020-12-15 20:57:31 +0800

    斐波那契数列 init

---
+++ src/Arrays/Fibonacci.java
@@ -0,0 +1,15 @@
+package Arrays;
+
+/**
+ * Create by SunnyDay on 2020/12/15 20:45
+ * 题目：斐波那契数列
+ * 介绍：https://leetcode-cn.com/problems/fei-bo-na-qi-shu-lie-lcof/
+ */
+public class Fibonacci {
+    public static void main(String[] args) {
+        System.out.println("test:"+(1%1000000007));
+    }
+    public int fib(int n) {
+        return n;
+    }
+}
