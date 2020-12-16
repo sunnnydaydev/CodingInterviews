567f9bed788c242b4d057578f837e4fef00ab6ff
Administrator <1246771571@qq.com>
2020-12-16 20:28:58 +0800

    斐波那契数列第一版解法：
    循环遍历法，时间复杂度o（n）

--- src/Arrays/Fibonacci.java
+++ src/Arrays/Fibonacci.java
@@ -6,10 +6,31 @@ package Arrays;
  * 介绍：https://leetcode-cn.com/problems/fei-bo-na-qi-shu-lie-lcof/
  */
 public class Fibonacci {
+    // 0,1,1,2,3,5,8,,,,
     public static void main(String[] args) {
-        System.out.println("test:"+(1%1000000007));
+         System.out.println("fib->Loop:"+fib(0));
+         System.out.println("fib->Loop:"+fib(1));
+         System.out.println("fib->Loop:"+fib(2));
+         System.out.println("fib->Loop:"+fib(3));
+         System.out.println("fib->Loop:"+fib(4));
+         System.out.println("fib->Loop:"+fib(5));
+         System.out.println("fib->Loop:"+fib(6));
     }
-    public int fib(int n) {
-        return n;
+
+    // 循环写法
+    public static int fib(int n) {
+        if (0 == n) return 0;
+        if (1 == n) return 1;
+
+        int preOne = 0;
+        int preTwo = 1;
+        int fn = 0;
+
+        for (int i = 2; i <= n; i++) {//从第三个数开始遍历计算fn的值。
+            fn = (preOne + preTwo) % 1000000007; // 计算fn的值
+            preOne = preTwo;
+            preTwo = fn;
+        }
+        return fn;
     }
 }
