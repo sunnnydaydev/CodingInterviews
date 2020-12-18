4ef710f268664a273981b5ceff22b20faea26f35
Administrator <1246771571@qq.com>
2020-12-18 20:23:11 +0800

    add 斐波那契数列递归写法（ps：复杂度较高，不建议使用）

--- src/loop/Fibonacci.java
+++ src/loop/Fibonacci.java
@@ -8,13 +8,21 @@ package loop;
 public class Fibonacci {
     // 0,1,1,2,3,5,8,,,,
     public static void main(String[] args) {
-         System.out.println("fib->Loop:"+fib(0));
-         System.out.println("fib->Loop:"+fib(1));
-         System.out.println("fib->Loop:"+fib(2));
-         System.out.println("fib->Loop:"+fib(3));
-         System.out.println("fib->Loop:"+fib(4));
-         System.out.println("fib->Loop:"+fib(5));
-         System.out.println("fib->Loop:"+fib(6));
+        System.out.println("fib->Loop:" + fib(0));
+        System.out.println("fib->Loop:" + fib(1));
+        System.out.println("fib->Loop:" + fib(2));
+        System.out.println("fib->Loop:" + fib(3));
+        System.out.println("fib->Loop:" + fib(4));
+        System.out.println("fib->Loop:" + fib(5));
+        System.out.println("fib->Loop:" + fib(6));
+        System.out.println("---------------");
+        System.out.println("fib->Recursion:"+fibRecursion(0));
+        System.out.println("fib->Recursion:"+fibRecursion(1));
+        System.out.println("fib->Recursion:"+fibRecursion(2));
+        System.out.println("fib->Recursion:"+fibRecursion(3));
+        System.out.println("fib->Recursion:"+fibRecursion(4));
+        System.out.println("fib->Recursion:"+fibRecursion(5));
+        System.out.println("fib->Recursion:"+fibRecursion(6));
     }
 
     // 循环写法
@@ -34,6 +42,23 @@ public class Fibonacci {
         return fn;
     }
     // 递归写法：时间复杂度太高
+
+    public static int fibRecursion(int n) {
+        if (0 == n) return 0;
+        if (1 == n) return 1;
+        return calculateFib(n, 2, 0, 1);
+    }
+
+    // 从索引为2的位置递归计算
+    private static int calculateFib(int n, int start, int preOne, int preTwo) {
+        int fn = (preOne + preTwo) % 1000000007; // 计算fn的值
+        if (start == n) {
+            return fn;
+        } else {
+            start++;
+            return calculateFib(n, start, preTwo, fn);// 更改 preOne、preTwo。递归遍历。
+        }
+    }
 
     // 数学公式：一般不需要。
 }
